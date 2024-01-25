0604dffa615bb3fff850ccec3c19b33df7e453c0
Gabriel Sales <gabrielsales081@gmail.com>
2024-01-25 16:33:40 -0300

    feat: create and styling login page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,35 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button, Input, Label } from '@/components'
+
 export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <h1>Sign-in</h1>
+
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <fieldset className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" />
+            </fieldset>
+
+            <Button className="w-full" type="submit">
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
