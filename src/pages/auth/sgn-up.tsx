598724a5419bbe08d88d80478059f532c7cbb8b9
Gabriel Sales <gabrielsales081@gmail.com>
2024-01-25 19:49:18 -0300

    feat: add sign-up page and styling

---
+++ src/pages/auth/sgn-up.tsx
@@ -0,0 +1,110 @@
+import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import { Button, Input, Label } from '@/components'
+
+const signUpForm = z.object({
+  restaurantName: z.string(),
+  managerName: z.string(),
+  phone: z.string(),
+  email: z.string().email(),
+})
+
+type TSignUpForm = z.infer<typeof signUpForm>
+
+export function SignUp() {
+  const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<TSignUpForm>()
+
+  async function handleSignUp(data: TSignUpForm) {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Restaurante cadastrado com sucesso', {
+        action: {
+          label: 'Login',
+          onClick: () => navigate('/sign-in'),
+        },
+      })
+      console.log(data)
+    } catch (error) {
+      toast.error('Erro ao cadastrar estabelecimento.')
+    }
+  }
+
+  return (
+    <>
+      <Helmet title="Cadastro" />
+
+      <div className="p-8">
+        <Link to="/sign-in">
+          <Button variant="ghost" className="absolute right-8 top-8 ">
+            Já tenho um cadastro
+          </Button>
+        </Link>
+
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Criar conta grátis
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Seja um parceiro e comece suas vendas!
+            </p>
+          </div>
+
+          <form onSubmit={handleSubmit(handleSignUp)} className="space-y-4">
+            <fieldset className="space-y-2">
+              <Label htmlFor="restaurantName">Nome do restaurante</Label>
+              <Input
+                id="restaurantName"
+                type="text"
+                {...register('restaurantName')}
+              />
+            </fieldset>
+            <fieldset className="space-y-2">
+              <Label htmlFor="managerName">Seu nome</Label>
+              <Input
+                id="managerName"
+                type="text"
+                {...register('managerName')}
+              />
+            </fieldset>
+            <fieldset className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </fieldset>
+            <fieldset className="space-y-2">
+              <Label htmlFor="phone">Seu celular</Label>
+              <Input id="phone" type="tel" {...register('phone')} />
+            </fieldset>
+
+            <Button disabled={isSubmitting} className="w-full" type="submit">
+              Finalizar cadastro
+            </Button>
+
+            <p className="px-6 text-center text-sm leading-relaxed text-muted-foreground">
+              Ao continuar você concorda com nossos{' '}
+              <a className="underline underline-offset-4" href="">
+                termos de serviço
+              </a>{' '}
+              e{' '}
+              <a className="underline underline-offset-4" href="">
+                politicas de privacidade
+              </a>
+              .
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -19,15 +20,19 @@ export function SignIn() {
   } = useForm<TSignInForm>()
 
   async function handleSignIn(data: TSignInForm) {
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
 
-    toast.success('Enviamos um link de autenticação para seu e-mail!', {
-      action: {
-        label: 'Reenviar',
-        onClick: () => handleSignIn(data),
-      },
-    })
-    console.log(data)
+      toast.success('Enviamos um link de autenticação para seu e-mail!', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+      console.log(data)
+    } catch (error) {
+      toast.error('Erro ao mandar e-mail')
+    }
   }
 
   return (
@@ -35,6 +40,12 @@ export function SignIn() {
       <Helmet title="Login" />
 
       <div className="p-8">
+        <Link to="/sign-up">
+          <Button variant="ghost" className="absolute right-8 top-8 ">
+            Registre seu estabelecimento
+          </Button>
+        </Link>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
